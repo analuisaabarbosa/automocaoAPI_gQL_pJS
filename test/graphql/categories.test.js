aafc41f676a5c6f71aeb016c66624f7efddc18b7
analuisa <analuisaaugustob@gmail.com>
2024-09-27 11:57:07 -0300

    Refatorando código.

--- test/graphql/categories.test.js
+++ test/graphql/categories.test.js
@@ -1,7 +1,10 @@
 const { spec } = require('pactum');
+const { like } = require('pactum-matchers');
 
+let token;
 beforeEach(async () => {
-    const response = await spec()
+
+    token = await spec()
         .post('http://lojaebac.ebaconline.art.br/graphql')
         .withGraphQLQuery(`
             mutation AuthUser($email: String, $password: String) {
@@ -17,13 +20,13 @@ beforeEach(async () => {
         })
         .returns('data.authUser.token')
 
-        token = response;
 });
+
 
 it('Should add a category in the list', async () => {
     await spec()
         .post('http://lojaebac.ebaconline.art.br/graphql')
-        .withHeaders("Authorization", `Bearer ${token}`)
+        .withHeaders("Authorization", token)
         .withGraphQLQuery(`
             mutation AddCategory($name: String, $photo: String) {
                 addCategory(name: $name, photo: $photo) {
@@ -36,12 +39,22 @@ it('Should add a category in the list', async () => {
             "name": "Bags",
             "photo": "https://www.zipmaster.com/wp-content/uploads/2022/04/Reusable-Cloth-Shopping-Bags-Rainbow-Pack-200-Case-Reusable-Bags-B26-061-3-1000x1000.jpg.webp"
         })
-        .expectStatus(200);
+        .returns('data._id')
+        .expectStatus(200)
+        .expectJsonMatch({
+            data: {
+                addCategory: {
+                    name: like("Bags"),
+                    photo: like("https://www.zipmaster.com/wp-content/uploads/2022/04/Reusable-Cloth-Shopping-Bags-Rainbow-Pack-200-Case-Reusable-Bags-B26-061-3-1000x1000.jpg.webp")
+                }
+            }
+        })
 });
 
 it('Should delete a category', async () => {
     await spec()
         .post('http://lojaebac.ebaconline.art.br/graphql')
+        .withHeaders("Authorization", token)
         .withGraphQLQuery(`
         mutation DeleteCategory($deleteCategoryId: ID!) {
             deleteCategory(id: $deleteCategoryId) {
@@ -50,14 +63,22 @@ it('Should delete a category', async () => {
        }        
     `)
         .withGraphQLVariables({
-            "deleteCategoryId": "66f567bc4252422517015341"
+            "deleteCategoryId": "66f6c66461c944a8eede896d"
         })
-        .expectStatus(200);
+        .expectStatus(200)
+        .expectJsonMatch({
+            data: {
+                deleteCategory: {
+                    name: null 
+                }
+            }
+        });
 });
 
 it('Should edit a category', async () => {
     await spec()
         .post('http://lojaebac.ebaconline.art.br/graphql')
+        .withHeaders("Authorization", token)
         .withGraphQLQuery(`
             mutation EditCategory($editCategoryId: ID!, $name: String, $photo: String) {
                 editCategory(id: $editCategoryId, name: $name, photo: $photo) {
@@ -67,9 +88,18 @@ it('Should edit a category', async () => {
         }
     `)
         .withGraphQLVariables({
-            "editCategoryId": "66d9fc36a60ca1ef7fa05cef",
-            "name": "Iphone 16",
-            "photo": null
+            "editCategoryId": "66f5aced42524225170155b4",
+            "name": `Viagens${Math.floor(Math.random() * 100)}`,
+            "photo": "https://www.gstatic.com/webp/gallery/1.sm.webp"
         })
         .expectStatus(200)
+        .expectJson({
+            data: {
+                editCategory: {
+                    name: null,  
+                    photo: null
+                }
+            }
+        })
 });
+
