0f1a98abf80fd761171be9d16fec9c7d2df7657a
analuisa <analuisaaugustob@gmail.com>
2024-09-28 11:20:36 -0300

    Executando testes do script.

--- test/contract/productsProvider.test.js
+++ test/contract/productsProvider.test.js
@@ -1,10 +1,29 @@
-const { spec } = require('pactum');
+const { reporter, flow } = require('pactum');
+const pf = require('pactum-flow-plugin');
 const { faker } = require('@faker-js/faker');
+
+function addFlowReporter() {
+    pf.config.url = 'http://localhost:8080';
+    pf.config.projectId = 'lojaebac_api';
+    pf.config.projectName = 'Loja Ebac API';
+    pf.config.version = '1.0.0';
+    pf.config.username = 'scanner';
+    pf.config.password = 'scanner';
+    reporter.add(pf.reporter);
+}
+
+before(async () => {
+    addFlowReporter();
+});
+
+after(async () => {
+    await reporter.end();
+});
 
 let token;
 beforeEach(async () => {
 
-    token = await spec()
+    token = await flow("Login")
         .post('http://lojaebac.ebaconline.art.br/public/authUser')
         .withJson({
             "email": "admin@admin",
@@ -18,7 +37,7 @@ it('API - Should add a product', async () => {
 
     const productName = faker.commerce.product()
 
-    await spec()
+    await flow("Add product")
         .post('http://lojaebac.ebaconline.art.br/api/addProduct')
         .withHeaders('Authorization', token)
         .withJson({
@@ -35,12 +54,12 @@ let id;
 it('API - Should delete a product', async () => {
 
     // Pegando o id do segundo produto da lista para poder deletar
-    id = await spec()
+    id = await flow("Take id of a product")
         .get('http://lojaebac.ebaconline.art.br/public/getProducts')
         .withHeaders('Authorization', token)
         .returns('products[1]._id')
 
-    await spec()
+    await flow("Delete product")
         .delete(`http://lojaebac.ebaconline.art.br/api/deleteProduct/${id}`)
         .withHeaders('Authorization', token)
         .expectStatus(200)
@@ -52,12 +71,12 @@ it('API - Should edit a product', async () => {
     const productName = faker.commerce.product()
 
     // Pegando o id do terceiro produto da lista para poder editar
-    id = await spec()
+    id = await flow("Take id of a product")
         .get('http://lojaebac.ebaconline.art.br/public/getProducts')
         .withHeaders('Authorization', token)
         .returns('products[2]._id')
 
-    await spec()
+    await flow("Edit product")
         .put(`http://lojaebac.ebaconline.art.br/api/editProduct/${id}`)
         .withHeaders('Authorization', token)
         .withJson({
