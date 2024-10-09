7e1a971e47a262c9807bc3f5f9dfaecef6761d0b
analuisa <analuisaaugustob@gmail.com>
2024-10-09 12:18:13 -0300

    Ajustes de formatação.

--- test/contract/categoriesConsumer.test.js
+++ test/contract/categoriesConsumer.test.js
@@ -9,7 +9,7 @@ function addFlowReporter() {
     pf.config.url = 'http://localhost:8080';
     pf.config.projectId = 'lojaebac_front_categories';
     pf.config.projectName = 'Loja Ebac Front - Categories';
-    pf.config.version = '1.0.2';
+    pf.config.version = '1.0.0';
     pf.config.username = 'scanner';
     pf.config.password = 'scanner';
     reporter.add(pf.reporter);
